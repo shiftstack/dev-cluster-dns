01fcf8fce4532ba22afa1854229401d0b5d983ab
Stephen Finucane <stephenfin@redhat.com>
2025-07-10 14:27:17 +0100

    Fix typo
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- cmd/create.go
+++ cmd/create.go
@@ -47,7 +47,7 @@ var createCmd = &cobra.Command{
 	Short: "Create or update records for a cluster",
 	Long: `Create or update DNS records for an OpenShift cluster in an existing HostedZone.
 
-$ cluster-dns create my-cluster --api 1.2.3.4 --ingress 1.2.3.5`,
+$ dev-cluster-dns create my-cluster --api 1.2.3.4 --ingress 1.2.3.5`,
 	Args: cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.TODO()
